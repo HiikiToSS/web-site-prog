1022a9a1e1bf7390879d24c8bfbafa2d21823592
that_who_fighting_for_2D <101421494+HiikiToSS@users.noreply.github.com>
2022-06-08 23:44:31 +0000

    Пытаюсь изменять Скрыть на Показать при нажатии кнопки скрывающей/показывающей элементы верхней панели навигации (ну нихрена ты панописал тут)

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 //alert('Доборо пожаловать на сайт оНяМеЧнИкОв когда-нибудь я сделаю это приветсткие нормальным а по что имеем - то имеем -_-')//
-
+var isTrue = true
+var isTrue2 = true
 function hide_up_nav(){
-  var upper_nav = document.getElementsByTagName('border_navi')
-  upper_nav.style['display'] = 'none'  
+  var upper_nav = document.getElementById('border_navi')
+
+if (isTrue2){
+  var set_other_hide = document.getElementById('set_hide_nav_to_end')
+  set_other_hide.innerHTML = "<div id='set_hide_nav_to_end'><button onclick = 'hide_up_nav()'>Показать верхнюю панель навигации  </button></div>"
+  isTrue2 = false
+}
+else{
+  //Ты пытался сделать изменение надписи при скрытии верхней панельки навигации
+}
+
+  if (isTrue){
+    upper_nav.style['display'] = 'none'
+    isTrue = false
+    
+  } 
+  else{
+    upper_nav.style['display'] = 'flex' 
+    isTrue = true
+  } 
 }
